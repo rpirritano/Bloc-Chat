805c336d4578d6906c5cefd582a201d834e4c3ea
rpirritano <rpirritano@gmail.com>
2017-02-13 08:21:36 -0800

    fixed firebase to show messages

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,16 +1,16 @@
 (function() {
     function Message($firebaseArray) {
-        var messageRef = firebase.database().ref().child("messages");
+        //var messageRef = firebase.database().ref().child("messages");
         var messageReference = firebase.database().ref().child("messages").orderByChild('roomId');
         
-        var messages = $firebaseArray(messageRef);
+        //var messages = $firebaseArray(messageRef);
         
-            Message.getByRoomId = function(roomId) {              
-                return $firebaseArray(messageReference.equalTo(roomId));
-            }
+        Message.getByRoomId = function(roomId) {              
+            return $firebaseArray(messageReference.equalTo(roomId));
+        }
         
         return Message;
-            }
+    }
         
     
     angular
